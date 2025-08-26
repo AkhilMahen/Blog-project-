dc23dd4ad4bf144af13fc50c0ac15af1b07eaa21
AkhilMahen <akhilmahendiran@gmail.com>
2025-08-26 11:13:36 +1000

    Added page router and navigation bar at homw pagw

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Blog from './pages/Blog'
+import Home from './pages/home'
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+       <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
     </div>
   )
 }
